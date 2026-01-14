91e76f86dc45697c928f03058155c2c9b59d36ae
Anthony Tran <qquang269@gmail.com>
2026-01-14 10:31:52 +0700

    Add test_withLatestTrue_emitsLatestToNewSubscriber and remove unnecessary break

--- Tests/ScreenStatetKitTests/AsyncStream/StreamProducerTests.swift
+++ Tests/ScreenStatetKitTests/AsyncStream/StreamProducerTests.swift
@@ -24,9 +24,27 @@ struct StreamProducerTests {
         var received: Int?
         for await element in await sut.stream {
             received = element
+        }
+
+        #expect(received == 42)
+    }
+
+    @Test("withLatest true emits latest element to new subscriber")
+    func test_withLatestTrue_emitsLatestToNewSubscriber() async {
+        let sut = StreamProducer<Int>(withLatest: true)
+
+        // Emit multiple elements before subscribing
+        await sut.emit(element: 1)
+        await sut.emit(element: 2)
+        await sut.emit(element: 99)
+
+        // New subscriber should receive the latest (99), not all previous
+        var received: Int?
+        for await element in await sut.stream {
+            received = element
             break
         }
-        
-        #expect(received == 42)
+
+        #expect(received == 99)
     }
 }
