99d5e1fb7c0bc7ce3df6b311c616198381359b01
Anthony Tran <qquang269@gmail.com>
2026-03-19 15:31:20 +0700

    Replace Task.sleep with withMainSerialExecutor for deterministic tests

--- Tests/ScreenStatetKitTests/Helpers/CancelBagTests.swift
+++ Tests/ScreenStatetKitTests/Helpers/CancelBagTests.swift
@@ -4,6 +4,7 @@
 //
 
 import Testing
+import ConcurrencyExtras
 @testable import ScreenStateKit
 
 @Suite("CancelBag Tests")
@@ -13,121 +14,125 @@ struct CancelBagTests {
 
     @Test("cancelAll cancels all stored tasks")
     func test_cancelAll_cancelsAllStoredTasks() async throws {
-        let sut = CancelBag(onDuplicate: .cancelExisting)
+        await withMainSerialExecutor {
+            let sut = CancelBag(onDuplicate: .cancelExisting)
 
-        let task1 = Task {
-            try await Task.sleep(for: .seconds(10))
+            let task1 = Task {
+                try await Task.sleep(for: .seconds(10))
+            }
+            let task2 = Task {
+                try await Task.sleep(for: .seconds(10))
+            }
+
+            task1.store(in: sut)
+            task2.store(in: sut)
+
+            await Task.megaYield()
+
+            await sut.cancelAll()
+
+            #expect(task1.isCancelled == true)
+            #expect(task2.isCancelled == true)
         }
-        let task2 = Task {
-            try await Task.sleep(for: .seconds(10))
-        }
-
-        task1.store(in: sut)
-        task2.store(in: sut)
-
-        try await Task.sleep(for: .milliseconds(50))
-
-        await sut.cancelAll()
-
-        try await Task.sleep(for: .milliseconds(50))
-
-        #expect(task1.isCancelled == true)
-        #expect(task2.isCancelled == true)
     }
 
     // MARK: - cancel(forIdentifier:) Tests
 
     @Test("cancel for identifier cancels specific task")
     func test_cancelForIdentifier_cancelsSpecificTask() async throws {
-        let sut = CancelBag(onDuplicate: .cancelExisting)
+        await withMainSerialExecutor {
+            let sut = CancelBag(onDuplicate: .cancelExisting)
 
-        let task1 = Task {
-            try await Task.sleep(for: .seconds(10))
+            let task1 = Task {
+                try await Task.sleep(for: .seconds(10))
+            }
+            let task2 = Task {
+                try await Task.sleep(for: .seconds(10))
+            }
+
+            task1.store(in: sut, withIdentifier: "task1")
+            task2.store(in: sut, withIdentifier: "task2")
+
+            await Task.megaYield()
+
+            await sut.cancel(forIdentifier: "task1")
+
+            #expect(task1.isCancelled == true)
+            #expect(task2.isCancelled == false)
         }
-        let task2 = Task {
-            try await Task.sleep(for: .seconds(10))
-        }
-
-        task1.store(in: sut, withIdentifier: "task1")
-        task2.store(in: sut, withIdentifier: "task2")
-
-        try await Task.sleep(for: .milliseconds(50))
-
-        await sut.cancel(forIdentifier: "task1")
-
-        try await Task.sleep(for: .milliseconds(50))
-
-        #expect(task1.isCancelled == true)
-        #expect(task2.isCancelled == false)
     }
 
     // MARK: - store() Tests
 
     @Test("store with same identifier cancels previous task")
     func test_store_withSameIdentifierCancelsPreviousTask() async throws {
-        let sut = CancelBag(onDuplicate: .cancelExisting)
+        await withMainSerialExecutor {
+            let sut = CancelBag(onDuplicate: .cancelExisting)
 
-        let task1 = Task {
-            try await Task.sleep(for: .seconds(10))
+            let task1 = Task {
+                try await Task.sleep(for: .seconds(10))
+            }
+            let task2 = Task {
+                try await Task.sleep(for: .seconds(10))
+            }
+
+            task1.store(in: sut, withIdentifier: "sameId")
+
+            await Task.megaYield()
+
+            task2.store(in: sut, withIdentifier: "sameId")
+
+            await Task.megaYield()
+
+            #expect(task1.isCancelled == true)
+            #expect(task2.isCancelled == false)
         }
-        let task2 = Task {
-            try await Task.sleep(for: .seconds(10))
-        }
-
-        task1.store(in: sut, withIdentifier: "sameId")
-
-        try await Task.sleep(for: .milliseconds(50))
-
-        task2.store(in: sut, withIdentifier: "sameId")
-
-        try await Task.sleep(for: .milliseconds(50))
-
-        #expect(task1.isCancelled == true)
-        #expect(task2.isCancelled == false)
     }
-    
+
     @Test("store with same identifier cancels new task")
     func test_store_withSameIdentifierCancelsNewTask() async throws {
-        let sut = CancelBag(onDuplicate: .cancelNew)
+        await withMainSerialExecutor {
+            let sut = CancelBag(onDuplicate: .cancelNew)
 
-        let task1 = Task {
-            try await Task.sleep(for: .seconds(10))
+            let task1 = Task {
+                try await Task.sleep(for: .seconds(10))
+            }
+            let task2 = Task {
+                try await Task.sleep(for: .seconds(10))
+            }
+
+            task1.store(in: sut, withIdentifier: "sameId")
+
+            await Task.megaYield()
+
+            task2.store(in: sut, withIdentifier: "sameId")
+
+            await Task.megaYield()
+
+            #expect(task1.isCancelled == false)
+            #expect(task2.isCancelled == true)
         }
-        let task2 = Task {
-            try await Task.sleep(for: .seconds(10))
-        }
-
-        task1.store(in: sut, withIdentifier: "sameId")
-
-        try await Task.sleep(for: .milliseconds(50))
-
-        task2.store(in: sut, withIdentifier: "sameId")
-
-        try await Task.sleep(for: .milliseconds(50))
-
-        #expect(task1.isCancelled == false)
-        #expect(task2.isCancelled == true)
     }
-    
-    @Test("watch task is copmpleted should remove it from cancelbag storage")
-    func testWatchTaskCompletedRemoveCancellerFromStorage() async throws {
-        let sut = CancelBag(onDuplicate: .cancelExisting)
 
-        Task {
-            try await Task.sleep(for: .milliseconds(10))
-        }.store(in: sut)
-        
-        Task {
-            try await Task.sleep(for: .seconds(10))
-        }.store(in: sut)
-        
-        try await Task.sleep(for: .milliseconds(100))
-        
-        let count = await sut.count
-        let isEmpty = await sut.isEmpty
-        
-        #expect(count == 1)
-        #expect(isEmpty == false)
+    @Test("watch task completed should remove it from cancelbag storage")
+    func testWatchTaskCompletedRemoveCancellerFromStorage() async throws {
+        await withMainSerialExecutor {
+            let sut = CancelBag(onDuplicate: .cancelExisting)
+
+            Task { }.store(in: sut)
+
+            Task {
+                try await Task.sleep(for: .seconds(10))
+            }.store(in: sut)
+
+            await Task.megaYield()
+
+            let count = await sut.count
+            let isEmpty = await sut.isEmpty
+
+            #expect(count == 1)
+            #expect(isEmpty == false)
+        }
     }
 }
 
